831343edfff9b626752950be90f06b4a3628ae17
Meet-Prajapati <159993984+Meet-Prajapati@users.noreply.github.com>
2024-12-25 23:20:52 +0530

    last commit

--- 01basic/src/App.js
+++ 01basic/src/App.js
@@ -1,9 +1,19 @@
-
+import Chai from "./chai";
 
 function App() {
   return (
+    <>
+    < Chai />
     <h1>Chai Ur React</h1>
+    <Vaibhav />
+    </>
   );
 }
 
 export default App;
+
+let Vaibhav = () =>{
+  return(
+    <h1> Vaibhav Prajapati</h1>
+  )
+}
